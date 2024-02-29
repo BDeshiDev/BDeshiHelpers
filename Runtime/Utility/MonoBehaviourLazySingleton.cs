29aec7eecccbc4cbfa8f1f2fe94377854066d47c
B-DeshiDev <ffalsoy@gmail.com>
2024-02-29 14:26:48 +0600

    Merge pull request #5 from BDeshiDev/dev
    Fix editor issue

---
+++ Runtime/Utility/MonoBehaviourLazySingleton.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+namespace Bdeshi.Helpers.Utility
+{
+    public class MonoBehaviourLazySingleton<T> : MonoBehaviour
+        where T : MonoBehaviourLazySingleton<T> 
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+
+                    if (_appicationQuitting)
+                    {
+                        # if UNITY_EDITOR
+                            //this never fires in the editor but causes issue with domain reload
+                            _appicationQuitting = false;
+                        #else
+                            return null;
+                        #endif
+                    }
+
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject(typeof(T).ToString());
+                        //obj.hideFlags = HideFlags.HideAndDontSave;
+                        _instance = obj.AddComponent<T>();
+                        _instance.Initialize();
+                        #if UNITY_EDITOR
+                        _instance.gameObject.name = _instance.GoName;
+                        #endif
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private static bool _appicationQuitting;
+
+        public virtual string GoName => typeof(T).ToString();
+        /// <summary>
+        /// Initialize is called on awake only if this is the first instance
+        /// </summary>
+        protected virtual void Initialize() { }
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if(_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+        private void OnApplicationQuit()
+        {
+            _appicationQuitting = true;
+        }
+
+        public static void PlayModeEnterCleanup()
+        {
+            _appicationQuitting = false;
+
+            if (_instance != null)
+                _instance.PlayModeEnterCleanupInternal();
+        }
+        
+        public static void PlayModeExitCleanup()
+        {
+            _instance = null;
+        }
+        
+        protected virtual void PlayModeEnterCleanupInternal()
+        {
+
+        }
+    }
+}
---
+++ Runtime/Utility/PooledMonoBehaviorList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bdeshi.Helpers.Utility
+{
+    [Serializable]
+    public class PooledMonoBehaviorList<T>
+    where T: MonoBehaviour
+    {
+        private List<T> _list = new ();
+        public List<T> List => _list;
+        private SimpleManualMonoBehaviourPool<T> _pool;
+        public SimpleManualMonoBehaviourPool<T> Pool => _pool;
+        
+        public PooledMonoBehaviorList(T prefab, int initialPooledCount = 0, Transform spawnParent = null)
+        {
+            _pool = new SimpleManualMonoBehaviourPool<T>(prefab, initialPooledCount, spawnParent);
+        }
+
+        /// <summary>
+        /// If list has enough elements, return element at index i
+        /// else, add elements to list until we have enough, then return element at index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T Get(int index)
+        {
+            if (_list.Count <= index)
+            {
+                _pool.EnsureSpawnListCount(_list, index+1);
+            }
+
+            return _list[index];
+        }
+        
+        public T GetNewItem()
+        {
+            var item = _pool.GetItem();
+            _list.Add(item);
+            return item;
+        }
+        
+        public void ClearAndReturnToPool()
+        {
+            foreach (var item in _list)
+            {
+                _pool.ReturnItem(item);
+            }
+            _list.Clear();
+        }
+        
+        public void ClearAndForget(int index)
+        {
+            _list.Clear();
+        }
+        
+        public void EnsureCount(int count)
+        {
+            _pool.EnsureSpawnListCount(_list, count);
+        }
+        public void EnsureCount(int count, Action<T> addedCallback, Action<T> removedCallback)
+        {
+            _pool.EnsureSpawnListCount(_list, count,addedCallback,removedCallback);
+        }
+    }
+}
